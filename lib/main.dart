6d60bf7eaaffbb7207e7a5fe0ce3b9ce45b85e30
nouramer725 <nouramer725@gmail.com>
2025-12-20 19:04:40 +0200

    initialRoute

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.dark,
       theme: AppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
-      initialRoute: AppRoutes.homeScreenName,
+      initialRoute: AppRoutes.onBoardingScreenName,
       routes: {
         AppRoutes.homeScreenName: (context) => HomeScreen(),
         AppRoutes.onBoardingScreenName: (context) => OnboardingScreen(),
